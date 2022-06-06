99c1f6e7b9a995192be2315e72297b7c64cb64aa
alfonsodcf <alfonsoservizi01@gmail.com>
2022-06-06 16:50:42 +0200

    index: h2

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 class Movie {
-    public $titolo;
-    public $genere;
-    public $release;
-    public $director;   
+    private $titolo;
+    private $genere;
+    private $release;
+    private $director;   
 
     function __construct ($_titolo, $_genere, $_release, $_director)
     {
@@ -11,6 +11,11 @@ class Movie {
         $this->genere = $_genere;
         $this->release = $_release;
         $this->director = $_director;
+    }
+
+    public function getTitolo()
+    {
+        return $this->titolo;
     }
 }
 
@@ -20,3 +25,17 @@ $movie2 = new Movie ('Le Iene', 'gangster', 1992, 'Quentin Tarantino');
 var_dump($movie1);
 var_dump($movie2);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2><?php echo $movie1->getTitolo();?></h2>
+    <h2><?php echo $movie2->getTitolo();?></h2>
+</body>
+</html>
